1d9329ba49ecb28600744a9ba5ff4b4523c13363
Mikjail Salazar <mikjailsalazar@gmail.com>
2020-06-26 23:00:30 +0400

    Config - Adding first configuration set

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/psicolab-demo/' :
+    '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@/styles/_variables.scss"; @import "@/styles/_mixins.scss";',
+      },
+    },
+  },
+};
